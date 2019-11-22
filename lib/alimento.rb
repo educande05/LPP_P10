ccf77749c94c4515ec149dfbb36b238c61634d03
Eduardo Pérez Suárez <alu0100890174@ull.edu.es>
2019-11-22 11:58:53 +0000

    Implementados Metodos para comprobar nodos de la lista

--- lib/alimento.rb
+++ lib/alimento.rb
@@ -1,59 +1,74 @@
 require "alimento/version"
 
-
+module Alimento
+  class Error < StandardError; end
+  # Your code goes here...
   class Alimento
+    attr_reader :nombre, :proteinas, :carbohidratos, :lipidos, :GEI, :terreno
+    def initialize(nombre, proteinas, carbohidratos, lipidos, gei, terreno)
+      @nombre = nombre
+      @proteinas = proteinas
+      @carbohidratos = carbohidratos
+      @lipidos = lipidos
+      @GEI = gei
+      @terreno = terreno
+    end
+    
+    def getNombre
+      @nombre
+    end
 
-  attr_accessor :nombre, :proteinas, :carbohidratos, :lipidos, :gei, :terreno, :hombre, :mujer
+    def getGEI
+      @GEI
+    end
 
-  def initialize  (nombre, proteinas, carbohidratos, lipidos, gei, terreno)
-    @nombre = nombre
-    @proteinas  = proteinas
-    @carbohidratos = carbohidratos
-    @lipidos  = lipidos
-    @gei  = gei
-    @terreno = terreno
-    @hombre = []
-    @mujer = []
+    def getTerreno
+      @terreno
+    end
 
+    def getProteinas
+      @proteinas
+    end
+
+    def getLipidos
+      @lipidos
+    end
+
+    def getCarbohidratos
+      @carbohidratos
+    end
+
+    def getValorCalorico
+      resultado = '%0.1f' % ((getProteinas * 4) + (getLipidos * 4) + (getCarbohidratos * 9))
+      return resultado.to_f
+    end
+
+    def to_s
+      "#{@nombre}"
+    end
+
+    def calcularCaloriasDieta(*valores_caloricos)
+      resultado = getValorCalorico
+      for valor in valores_caloricos
+        resultado += valor
+      end
+      return resultado.round(1)
+    end
+
+    def calcularGEI(*geis)
+      resultado = getGEI
+      for gei in geis
+        resultado += gei 
+      end
+      return resultado.round(2)
+    end
+
+    def calcularTerreno(*terrenos)
+      resultado = getTerreno
+      for terreno in terrenos
+        resultado += terreno 
+      end
+      return resultado.round(2)
+    end
   end
-
-  def to_s_name
-    "#{@nombre}"
-  end
-
-  def to_s_proteinas
-    "#{@proteinas}"
-  end
-
-  def to_s_gei
-    "#{@gei}"
-  end
-
-  def to_s_terreno
-    "#{@terreno}"
-  end
-
-  def clear
-    @proteinas  = 0
-    @carbohidratos = 0
-    @lipidos  =0
-    @gei  = 0
-    @terreno = 0
-    "El alimento #{@nombre} ahora está vacío"
-  end
-
-  def value_energy
-    (4*@proteinas+9*@lipidos+4*@carbohidratos).round(2)
-  end
-
-  def eq_gei (valor_mg)
-    factor=(100*valor_mg)/100000
-    valor=factor*@gei
-  end
-
-  def eq_terreno (valor_mg)
-    factor=(100*valor_mg)/100000
-    valor=factor*@terreno
-  end
-
 end
--- lib/lista.rb
+++ lib/lista.rb
@@ -24,3 +24,35 @@ module Alimento
     def empty
       head == tail && head.value == nil
     end
+
+    def size
+      if empty
+        0
+      elsif head == tail && head.value != nil
+        1
+      else
+        i = 1
+        aux = @head
+        while aux != @tail
+          i += 1
+          aux = aux.next
+        end
+        i
+      end
+    end
+
+    def size
+      if empty
+        0
+      elsif head == tail && head.value != nil
+        1
+      else
+        i = 1
+        aux = @head
+        while aux != @tail
+          i += 1
+          aux = aux.next
+        end
+        i
+      end
+    end
